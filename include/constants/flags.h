be94cbcb399d3cf4067ac25aaa72dc888ab3ccba
Drew Hughes <drew.james.hughes@gmail.com>
2024-01-23 13:17:16 -0500

    Manaphy done

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -53,7 +53,7 @@
 #define FLAG_ROTOM_CAUGHT    0x27
 #define FLAG_HIDE_REGIGIGAS  0x28 
 #define FLAG_DEFEATED_REGIGIGAS 0x29 
-#define FLAG_UNUSED_0x02A    0x2A // Unused Flag
+#define FLAG_HIDE_MANAPHY    0x2A
 #define FLAG_UNUSED_0x02B    0x2B // Unused Flag
 #define FLAG_UNUSED_0x02C    0x2C // Unused Flag
 #define FLAG_UNUSED_0x02D    0x2D // Unused Flag
