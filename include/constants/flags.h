d046f00980c12a9d4b7847d49632e59c0a61175e
Drew Hughes <drew.james.hughes@gmail.com>
2024-01-23 17:03:06 -0500

    meloetta done

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -56,7 +56,7 @@
 #define FLAG_HIDE_MANAPHY    0x2A
 #define FLAG_HIDE_PHIONE     0x2B 
 #define FLAG_RECEIVED_VICTINI   0x2C 
-#define FLAG_UNUSED_0x02D    0x2D // Unused Flag
+#define FLAG_HIDE_MELOETTA   0x2D
 #define FLAG_UNUSED_0x02E    0x2E // Unused Flag
 #define FLAG_UNUSED_0x02F    0x2F // Unused Flag
 #define FLAG_UNUSED_0x030    0x30 // Unused Flag
