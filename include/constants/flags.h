5c34f92b2e0dbc1bec9d8fcc2f8bf31e008dd312
Drew Hughes <drew.james.hughes@gmail.com>
2024-02-13 15:11:26 +0900

    Jirachi has bugs

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -66,9 +66,9 @@
 #define FLAG_CAUGHT_ZAPDOS   0x34 
 #define FLAG_HIDE_MOLTRES    0x35 
 #define FLAG_CAUGHT_MOLTRES  0x36 
-#define FLAG_UNUSED_0x037    0x37 // Unused Flag
-#define FLAG_UNUSED_0x038    0x38 // Unused Flag
-#define FLAG_UNUSED_0x039    0x39 // Unused Flag
+#define FLAG_IS_NIGHT        0x37
+#define FLAG_CAUGHT_JIRACHI  0x38 
+#define FLAG_HIDE_JIRACHI    0x39 
 #define FLAG_UNUSED_0x03A    0x3A // Unused Flag
 #define FLAG_UNUSED_0x03B    0x3B // Unused Flag
 #define FLAG_UNUSED_0x03C    0x3C // Unused Flag
