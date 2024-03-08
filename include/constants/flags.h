f06b53cea98542402b54c035f1b37f98998d72f7
Drew Hughes <drew.james.hughes@gmail.com>
2024-03-08 13:15:08 +0900

    Hoopa start

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -513,10 +513,10 @@
 #define FLAG_DEFEATED_HO_OH                  0x1DC
 #define FLAG_DEFEATED_LUGIA                  0x1DD
 
-#define FLAG_UNUSED_0x1DE                    0x1DE // Unused Flag
-#define FLAG_UNUSED_0x1DF                    0x1DF // Unused Flag
-#define FLAG_UNUSED_0x1E0                    0x1E0 // Unused Flag
-#define FLAG_UNUSED_0x1E1                    0x1E1 // Unused Flag
+#define FLAG_HIDE_HOOPA                      0x1DE
+#define FLAG_CAUGHT_HOOPA                    0x1DF
+#define FLAG_ALL_PORTALS_CLOSED              0x1E0
+#define FLAG_MET_HOOPA_ONCE                  0x1E1
 #define FLAG_UNUSED_0x1E2                    0x1E2 // Unused Flag
 #define FLAG_UNUSED_0x1E3                    0x1E3 // Unused Flag
 
