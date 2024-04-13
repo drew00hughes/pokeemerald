c9eb7d2588f60ad4ae8621b8cdc1261e02722357
Drew Hughes <drew.james.hughes@gmail.com>
2024-04-13 17:29:02 -0400

    No idea how to fix this

--- src/pokemon.c
+++ src/pokemon.c
@@ -5020,14 +5020,19 @@ void SetMonData(struct Pokemon *mon, s32 field, const void *dataArg)
 
 void SetBoxMonData(struct BoxPokemon *boxMon, s32 field, const void *dataArg)
 {
-    u16 test = 0;
+    u16 zeroOut = 0;
     const u8 *data = dataArg;
         switch (field)
         {
         case MON_DATA_SPECIES:
             SET16(boxMon->species);
         case MON_DATA_HELD_ITEM:
-            SET16(test);
+            //if (boxMon->species == boxMon->heldItem) {
+            //    SET16(zeroOut);
+            //}
+            //else {
+                SET16(boxMon->heldItem);
+            //}
             break;
         case MON_DATA_EXP:
             SET32(boxMon->experience);
